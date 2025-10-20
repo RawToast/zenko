5d3867af3bc836d1da783d80a2dae3e8bb01e38d
RawToast <8013000+RawToast@users.noreply.github.com>
2025-10-20 21:17:27 +0900

    phew

--- packages/zenko/src/zenko.ts
+++ packages/zenko/src/zenko.ts
@@ -691,10 +691,10 @@ function appendHelperTypesImport(
       buffer.push(
         "type OperationErrors<TClient = unknown, TServer = unknown, TDefault = unknown, TOther = unknown> = {"
       )
-      buffer.push("  client?: TClient")
-      buffer.push("  server?: TServer")
-      buffer.push("  default?: TDefault")
-      buffer.push("  other?: TOther")
+      buffer.push("  clientErrors?: TClient")
+      buffer.push("  serverErrors?: TServer")
+      buffer.push("  defaultErrors?: TDefault")
+      buffer.push("  otherErrors?: TOther")
       buffer.push("}")
       buffer.push(
         "type OperationDefinition<TMethod extends RequestMethod, TPath extends (...args: any[]) => string, TRequest = undefined, TResponse = undefined, THeaders extends AnyHeaderFn | undefined = undefined, TErrors extends OperationErrors | undefined = undefined> = {"
